e7f9bd70757b32bb5551628de2f26ab0ebd5a5c7
sarathantony <sarathantony@yahoo.co.in>
2019-11-10 22:29:38 +0530

    Updates

---
+++ precache-manifest.365024c24bc13ab8b6efbe7913e82bc7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccc80fd2edef7561fe4ac88da0dd38c",
+    "url": "/movies-list/index.html"
+  },
+  {
+    "revision": "b7c96c192af275b3e362",
+    "url": "/movies-list/static/css/main.86771b24.chunk.css"
+  },
+  {
+    "revision": "397b108300ffd555bace",
+    "url": "/movies-list/static/js/2.19ca7fec.chunk.js"
+  },
+  {
+    "revision": "b7c96c192af275b3e362",
+    "url": "/movies-list/static/js/main.8c6a2691.chunk.js"
+  },
+  {
+    "revision": "dfbb2584fd16f87ad9a8",
+    "url": "/movies-list/static/js/runtime-main.ba8f44ae.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/movies-list/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/movies-list/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/movies-list/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/movies-list/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/movies-list/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/movies-list/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/movies-list/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/movies-list/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/movies-list/static/media/poster9.65d6d500.jpg"
+  }
+]);
